15d5728a89d34d2d2f9a0b6337933efd9bc47688
Tyzbit <3319104+tyzbit@users.noreply.github.com>
2021-01-22 21:07:31 -0500

    stuff

--- newrelic/servers.tf
+++ newrelic/servers.tf
@@ -43,3 +43,69 @@ resource "newrelic_nrql_alert_condition" "high-disk-usage" {
     threshold_occurrences = "ALL"
   }
 }
+
+resource "newrelic_nrql_alert_condition" "high-memory-usage" {
+  account_id                   = data.aws_ssm_parameter.account-id.value
+  policy_id                    = newrelic_alert_policy.server-alerts.id
+  type                         = "static"
+  name                         = "Memory usage is high"
+  enabled                      = true
+  violation_time_limit_seconds = 3600
+  value_function               = "single_value"
+
+  fill_option = "none"
+
+  aggregation_window             = 60
+  expiration_duration            = 120
+  open_violation_on_expiration   = false
+  close_violations_on_expiration = false
+
+  nrql {
+    query             = <<EOF
+      FROM SystemSample
+      SELECT average(memoryUsedPercent)
+      FACET hostname
+      EOF
+    evaluation_offset = 3
+  }
+
+  critical {
+    operator              = "above"
+    threshold             = 90
+    threshold_duration    = 900
+    threshold_occurrences = "ALL"
+  }
+}
+
+resource "newrelic_nrql_alert_condition" "high-cpu-usage" {
+  account_id                   = data.aws_ssm_parameter.account-id.value
+  policy_id                    = newrelic_alert_policy.server-alerts.id
+  type                         = "static"
+  name                         = "CPU pegged above 90"
+  enabled                      = true
+  violation_time_limit_seconds = 3600
+  value_function               = "single_value"
+
+  fill_option = "none"
+
+  aggregation_window             = 60
+  expiration_duration            = 120
+  open_violation_on_expiration   = false
+  close_violations_on_expiration = false
+
+  nrql {
+    query             = <<EOF
+      FROM SystemSample
+      SELECT average(cpuPercent)
+      FACET hostname 
+      EOF
+    evaluation_offset = 3
+  }
+
+  critical {
+    operator              = "above"
+    threshold             = 90
+    threshold_duration    = 1800
+    threshold_occurrences = "ALL"
+  }
+}
