236566710f0bd075ceef95e7c7994e8dec56febc
Tyzbit <3319104+tyzbit@users.noreply.github.com>
2021-01-24 14:37:25 -0500

    move terraform file for newrelic

--- newrelic/main.tf
+++ newrelic/main.tf
@@ -11,7 +11,7 @@ terraform {
 
   backend "s3" {
     bucket         = "tyzbit.terraform"
-    key            = "synthetics.tfstate"
+    key            = "newrelic.tfstate"
     region         = "us-east-1"
     dynamodb_table = "tf-locking-table"
   }
