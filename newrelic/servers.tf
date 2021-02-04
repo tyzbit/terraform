04667e3d2957d493aae8ce6d1f3d73e80484dd93
Tyzbit <3319104+tyzbit@users.noreply.github.com>
2021-02-03 19:37:28 -0500

    add temperature checking

--- newrelic/servers.tf
+++ newrelic/servers.tf
@@ -100,3 +100,37 @@ module "server-port-checks" {
 
   job_comment = each.value.job_comment
 }
+
+resource "newrelic_nrql_alert_condition" "system-temp-above-90" {
+  account_id                   = data.aws_ssm_parameter.account-id.value
+  policy_id                    = newrelic_alert_policy.server-alerts.id
+  type                         = "static"
+  name                         = "System temperature above 90"
+  enabled                      = true
+  violation_time_limit_seconds = 3600
+  value_function               = "single_value"
+
+  fill_option = "none"
+
+  aggregation_window             = 60
+  expiration_duration            = 3600
+  open_violation_on_expiration   = true
+  close_violations_on_expiration = false
+
+  nrql {
+    query             = <<EOF
+      FROM Log
+      SELECT average(sensor_temperature)
+      WHERE job_comment = 'Check the temperature of thermal_zone2'
+      FACET hostname
+      EOF
+    evaluation_offset = 3
+  }
+
+  critical {
+    operator              = "above"
+    threshold             = 90
+    threshold_duration    = 300
+    threshold_occurrences = "ALL"
+  }
+}
